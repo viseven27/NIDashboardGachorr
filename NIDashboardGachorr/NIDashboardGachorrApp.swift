fdcc73952c0f146788fe6c5f5e9d76b557bef766
Alvin Justine <130334387+viseven27@users.noreply.github.com>
2025-07-22 05:44:57 +0700

    Initial Commit

---
+++ NIDashboardGachorr/NIDashboardGachorrApp.swift
@@ -0,0 +1,17 @@
+//
+//  NIDashboardGachorrApp.swift
+//  NIDashboardGachorr
+//
+//  Created by Alvin Justine on 22/07/25.
+//
+
+import SwiftUI
+
+@main
+struct NIDashboardGachorrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
